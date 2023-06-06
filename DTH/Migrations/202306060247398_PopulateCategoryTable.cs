d469c133592e9cca98205c333a58d8286259b948
hoa645 <dangthanhhoa102@gmail.com>
2023-06-06 17:17:13 +0700

    Add project files.

---
+++ DTH/Migrations/202306060247398_PopulateCategoryTable.cs
@@ -0,0 +1,20 @@
+﻿namespace DTH.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSER INTO CATEGORIES(ID,NAME) VALUES(1,'Development')");
+            Sql("INSER INTO CATEGORIES(ID,NAME) VALUES(2,'Business')");
+            Sql("INSER INTO CATEGORIES(ID,NAME) VALUES(1,'Marketing')");
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
